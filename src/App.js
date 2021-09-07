bd26902baf0a3db5c14e04cd24ef51757affa516
Unknown-word <superhappy244@gmail.com>
2021-09-07 12:42:46 +0900

    Step 2. useInput

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import React, {useState} from 'react';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button } from 'react-bootstrap';
+import React, { useState } from "react";
+import "./App.css";
+
+const useInput = (initialValue, validator) => {
+  const [value, setValue] = useState(initialValue);
+
+  const onChange = (event) => {
+
+    const {
+      target: { value },
+    } = event;
+
+    let willUpdate = true;
+
+    if (typeof validator === "function") {          // vali의 타입이 펑션인가?
+      willUpdate = validator(value);                // 펑션이라면  willUpdate의 타입을 vali가 가진 boolean 값으로 변경
+      console.log(willUpdate, validator);           // maxLen === validator
+    }
+
+    if (willUpdate) {
+      setValue(value); // willUpdate가 True라면 value를 업데이트
+    }
+  };
+
+  return { value, onChange }; // value 값을 리턴
+}
 
 const App = () => {
+  const maxLen = (value) => value.length <= 10; // 길이 제한 함수
 
-  const [item, setItem] = useState(0);
-  const incrementItem = () => setItem( item + 1 );
-  const decrementItem = () => setItem( item - 1 );
-
-  return <div>
-    <h1 className="title">Hello World</h1>
-    <h2>your select</h2>
-    <p>{item}</p>
-    <Button onClick={incrementItem}>+ 1</Button>  
-    <Button onClick={decrementItem}>- 1</Button>
-  </div>
+  const name = useInput("Mr.", maxLen); /* useInput의 value는 Mr. 가 되고, name은 value 값을 갖게 됨
+                                           maxLen으로 길이 제한 */
+  return (
+    <div>
+      <h1>Hello</h1>
+      <input placeholder="Name" {...name} />{" "}
+      {/* == value={name.value} onChange={name.onChange}*/}
+    </div>
+  );
 }
 
 export default App;
+
+// import React, {useState} from 'react';
+// import './App.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import { Button } from 'react-bootstrap';
+
+// const useInput = () => {
+
+// }
+
+// const App = () => {
+
+//   const [item, setItem] = useState(0);               <<< useState 함수형 컴포넌트
+//   const incrementItem = () => setItem( item + 1 );
+//   const decrementItem = () => setItem( item - 1 );
+
+//   return <div>
+//     <h1 className="title">Hello World</h1>
+//     <h2>your select</h2>
+//     <p>{item}</p>
+//     <Button onClick={incrementItem}>+ 1</Button>
+//     <Button onClick={decrementItem}>- 1</Button>
+//   </div>
+// }
+
+// export default App;
+
+/* ================================================== */
+
 // import React  from 'react';
 
 // class App extends React.Component {
@@ -34,12 +84,12 @@ export default App;
 //     }
 //     decrementItem = () => {
 //         this.setState({
-//           item:this.state.item -1
+//           item:this.state.item -1                                          <<< 클래스형 컴포넌트
 //         }
 //       )
 //     }
 
-//     render() { 
+//     render() {
 //         return (
 //             <div>
 //                 <p>{this.state.item}</p>
@@ -49,5 +99,5 @@ export default App;
 //         );
 //     }
 // }
- 
+
 // export default App;
